dc48703327d9e958fd5562178fff7cdb5c4b9288
Alex Daniel Motogna <alexdanielmotogna@gmail.com>
2025-09-18 18:15:19 +0200

    feat: vercel.json updated

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,8 @@
 import serverless from "serverless-http";
 import app from "./serverApp";
 
-// Exportar handler para Vercel
 export const handler = serverless(app);
 
-// Solo para desarrollo local
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
